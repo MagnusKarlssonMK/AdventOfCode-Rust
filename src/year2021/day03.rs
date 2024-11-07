33cead7af8c6d3822f7571fcf96eb256b52b8777
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-11-07 21:55:05 +0100

    2021 day 3 complete.

--- src/year2021/day03.rs
+++ src/year2021/day03.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashSet, usize};
+
 pub fn solve(input: &str) {
     let solution_data = InputData::parse_input(&input);
     println!("Part 1: {}", solution_data.solve_part1());
@@ -5,29 +7,86 @@ pub fn solve(input: &str) {
 }
 
 struct InputData {
-    numbers: Vec<String>,
-    width: usize
+    width: usize,
+    numbers: Vec<usize>,
 }
 
 impl InputData {
     fn parse_input(input: &str) -> Self {
-        let w = input.lines().nth(0).unwrap().len();
+        let nbrs: Vec<&str> = input.lines().collect();
         Self {
-            numbers: input.lines()
-                .map(|l| l.to_string())
-                .collect(),
-            width: w
+            width: nbrs[0].len(),
+            numbers: nbrs.iter().map(|n| usize::from_str_radix(&n, 2).unwrap()).collect(),
         }
     }
 
     fn solve_part1(&self) -> usize {
-        println!("{:?}", usize::from_str_radix(&self.numbers[0], 2).unwrap());
-        println!("{}", self.width);
-        1
+        let mut gamma: usize = 0;
+        let mut epsilon: usize = 0;
+        for bit in 0..self.width {
+            let mask: usize = 1 << bit;
+            let onecount: usize = self.numbers.iter().map(|nbr| if nbr & mask > 0 {1} else {0} ).sum();
+            if onecount > self.numbers.len() / 2 {
+                gamma |= mask;
+            } else {
+                epsilon |= mask;
+            }
+        }
+        gamma * epsilon
     }
 
     fn solve_part2(&self) -> usize {
-        2
+        let mut oxygen_candidates: HashSet<usize> = HashSet::new();
+        let mut scrubber_candidates: HashSet<usize> = HashSet::new();
+        for i in 0..self.numbers.len() {
+            oxygen_candidates.insert(i);
+            scrubber_candidates.insert(i);
+        }
+        // This can probably be made a lot shorter with hashmap instead of two hashsets, and also some way to re-use almost duplicated code.
+        for bit in (0..self.width).rev() {
+            let mask: usize = 1 << bit;
+            if oxygen_candidates.len() > 1 {
+                let mut ones: HashSet<usize> = HashSet::new();
+                let mut zeroes: HashSet<usize> = HashSet::new();
+                for c in oxygen_candidates.iter() {
+                    if self.numbers[*c] & mask > 0 {
+                        ones.insert(*c);
+                    } else {
+                        zeroes.insert(*c);
+                    }
+                }
+                if ones.len() >= zeroes.len() {
+                    for z in zeroes {
+                        oxygen_candidates.remove(&z);
+                    }
+                } else {
+                    for o in ones {
+                        oxygen_candidates.remove(&o);
+                    }
+                }
+            }
+            if scrubber_candidates.len() > 1 {
+                let mut ones: HashSet<usize> = HashSet::new();
+                let mut zeroes: HashSet<usize> = HashSet::new();
+                for c in scrubber_candidates.iter() {
+                    if self.numbers[*c] & mask > 0 {
+                        ones.insert(*c);
+                    } else {
+                        zeroes.insert(*c);
+                    }
+                }
+                if zeroes.len() > ones.len() {
+                    for z in zeroes {
+                        scrubber_candidates.remove(&z);
+                    }
+                } else {
+                    for o in ones {
+                        scrubber_candidates.remove(&o);
+                    }
+                }
+            }
+        }
+        self.numbers[*oxygen_candidates.iter().next().unwrap()] * self.numbers[*scrubber_candidates.iter().next().unwrap()]
     }
 }
 
