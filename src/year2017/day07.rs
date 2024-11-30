0cea75dc47870695b45bf982134960501b277b1d
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-11-30 17:52:50 +0100

    Complete solution for 2017 day 7.

--- src/year2017/day07.rs
+++ src/year2017/day07.rs
@@ -1,4 +1,3 @@
-#![allow(unused)]  // Remove me!!!
 use std::collections::HashMap;
 
 pub fn solve(input: &str) {
@@ -33,7 +32,6 @@ impl Program {
 
 struct InputData {
     programs: HashMap<String, Program>,
-    parents: HashMap<String, String>,
     root: String
 }
 
@@ -44,23 +42,71 @@ impl InputData {
                 let (name, rest) = line.split_once(' ').unwrap();
                 (name.to_string(), Program::from(rest))
             }).collect();
+        let mut parents: HashMap<String, String> = HashMap::new();
+        for (p_name, prog) in name_and_prog_list.iter() {
+            for leaf_name in prog.leafs.iter() {
+                parents.insert(leaf_name.clone(), p_name.clone());
+            }
+        }
+        let mut root = None;
+        for (p_name, _) in name_and_prog_list.iter() {
+            if !parents.contains_key(p_name) {
+                root = Some(p_name.clone());
+            }
+        }
         Self {
             programs: HashMap::from_iter(name_and_prog_list),
-            parents: HashMap::new(),
-            root: String::from("ROOT") }
-
-        // Todo: fill in parents and root (root is answer to part 1)
-        // Then onto part 2
-        // Remove compiler warning suppression
+            root: root.unwrap() }
     }
 
     fn solve_part1(&self) -> String {
-        println!("HUH: {:?}", self.programs["aaeeqh"]);
         self.root.clone()
     }
 
     fn solve_part2(&self) -> usize {
-        2
+        let (_, a) = self.get_weight_and_correction(&self.root);
+        a as usize
+    }
+
+    fn get_weight_and_correction(&self, prog: &str) -> (usize, isize) {
+        let mut leafweights: HashMap<usize, Vec<String>> = HashMap::new();
+        let mut leafcorrections: Vec<isize> = Vec::new();
+        if self.programs[prog].leafs.is_empty() {
+            return (self.programs[prog].weight, 0);
+        }
+
+        for leaf in self.programs[prog].leafs.iter() {
+            let (w, c) = self.get_weight_and_correction(leaf);
+            leafweights.entry(w)
+                .or_default()
+                //.or_insert_with(Vec::new)
+                .push(leaf.to_string());
+            leafcorrections.push(c);
+        }
+
+        let correction: isize = leafcorrections.iter().sum();
+        let leafcount: usize = leafweights.values().map(|v| v.len()).sum();
+        if correction != 0 {
+            // A node further down the tree has reported a correction, just propagate upwards
+            return (self.programs[prog].weight + (leafweights.keys().sum::<usize>() * leafcount),
+                    correction);
+        } else if leafweights.len() > 1 {
+            // These leafs are not balanced; figure out which one is bad and calculate the required correction
+            let mut correct_weight: usize = 0;
+            let mut bad_weight: usize = 0;
+            let mut bad_node = String::from("");
+            for (weight, nodes) in leafweights.iter() {
+                if nodes.len() > 1 {
+                    correct_weight = *weight;
+                } else {
+                    bad_weight = *weight;
+                    bad_node = nodes.first().unwrap().to_string();
+                }
+            }
+            return (self.programs[prog].weight + (correct_weight * leafcount),
+                    correct_weight as isize - (bad_weight as isize - self.programs[&bad_node].weight as isize));
+        }  // else - all leafs have reported the same weight and are thus balanced
+        (self.programs[prog].weight + (leafweights.keys().sum::<usize>() * leafcount), 0)
     }
 }
 
@@ -69,15 +115,41 @@ mod tests {
     use super::*;
     #[test]
     fn part1_example_1() {
-        let testdata = String::from("pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\nqoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\nugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)");
+        let testdata = String::from(
+"pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)");
         let solution_data = InputData::parse_input(&testdata);
         assert_eq!(solution_data.solve_part1(), String::from("tknk"));
     }
 
     #[test]
     fn part2_example_1() {
-        let testdata = String::from("pbga (66)\nxhth (57)\nebii (61)\nhavc (66)\nktlj (57)\nfwft (72) -> ktlj, cntj, xhth\nqoyq (66)\npadx (45) -> pbga, havc, qoyq\ntknk (41) -> ugml, padx, fwft\njptl (61)\nugml (68) -> gyxo, ebii, jptl\ngyxo (61)\ncntj (57)");
+        let testdata = String::from(
+"pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part2(), 10);
+        assert_eq!(solution_data.solve_part2(), 60);
     }
 }
