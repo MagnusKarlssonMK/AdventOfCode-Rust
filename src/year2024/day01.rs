760f8d81344d6e0827120ec29161ae312d30fd05
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-12-01 08:43:15 +0100

    2024 day 1 first version.

--- src/year2024/day01.rs
+++ src/year2024/day01.rs
@@ -5,25 +5,40 @@ pub fn solve(input: &str) {
 }
 
 struct InputData {
-    data: Vec<String>
+    left: Vec<usize>,
+    right: Vec<usize>
 }
 
 impl InputData {
     fn parse_input(input: &str) -> Self {
-        Self {
-            data: input.lines().map(|line| line.to_string()).collect()
+        let mut left: Vec<usize> = Vec::new();
+        let mut right: Vec<usize> = Vec::new();
+        for line in input.lines() {
+            let mut nbrs = line.split_whitespace();
+            left.push(nbrs.next().unwrap().parse().unwrap());
+            right.push(nbrs.next().unwrap().parse().unwrap());
         }
+        Self { left, right }
     }
 
     fn solve_part1(&self) -> usize {
-        for line in &self.data {
-            println!("{}", line);
-        }
-        1
+        let mut left = self.left.clone();
+        let mut right = self.right.clone();
+        left.sort_unstable();
+        right.sort_unstable();
+        left.iter()
+            .zip(right.iter())
+            .map(|(l, r)| l.abs_diff(*r))
+            .sum()
     }
 
     fn solve_part2(&self) -> usize {
-        2
+        self.left.iter()
+            .map(|left_nbr|
+                left_nbr * self.right.iter()
+                    .filter(|right_nbr| *right_nbr == left_nbr)
+                    .count())
+            .sum()
     }
 }
 
@@ -33,15 +48,27 @@ mod tests {
 
     #[test]
     fn part1_example_1() {
-        let testdata = String::from("1");
+        let testdata = String::from(
+"3   4
+4   3
+2   5
+1   3
+3   9
+3   3");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part1(), 1);
+        assert_eq!(solution_data.solve_part1(), 11);
     }
 
     #[test]
     fn part2_example_1() {
-        let testdata = String::from("2");
+        let testdata = String::from(
+"3   4
+4   3
+2   5
+1   3
+3   9
+3   3");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part2(), 2);
+        assert_eq!(solution_data.solve_part2(), 31);
     }
 }
