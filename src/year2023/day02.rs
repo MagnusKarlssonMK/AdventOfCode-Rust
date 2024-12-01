55009db8af7c7a28698024106f3a7f10a464ed54
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-12-01 21:43:43 +0100

    2023 day 2 - Add missing unit test input.

--- src/year2023/day02.rs
+++ src/year2023/day02.rs
@@ -100,15 +100,25 @@ mod tests {
 
     #[test]
     fn part1_example_1() {
-        let testdata = String::from("1");
+        let testdata = String::from(
+"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part1(), 1);
+        assert_eq!(solution_data.solve_part1(), 8);
     }
 
     #[test]
     fn part2_example_1() {
-        let testdata = String::from("2");
+        let testdata = String::from(
+"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part2(), 2);
+        assert_eq!(solution_data.solve_part2(), 2286);
     }
 }
