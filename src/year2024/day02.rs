fad86af5f1abb3f6256e622dfe31e93fef75b70d
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-12-02 20:20:11 +0100

    2024 day 2 - Run both parts in one sweep, some structure overhaul and cleanup.

--- src/year2024/day02.rs
+++ src/year2024/day02.rs
@@ -2,57 +2,82 @@ use std::cmp::Ordering;
 
 pub fn solve(input: &str) {
     let solution_data = InputData::parse_input(input);
-    println!("Part 1: {}", solution_data.solve_part1());
-    println!("Part 2: {}", solution_data.solve_part2());
+    let (p1, p2) = solution_data.solve();
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
+}
+
+#[derive(PartialEq)]
+enum SafetyLevel {
+    Safe,
+    DampenerSafe,
+    Unsafe
+}
+
+struct Report {
+    levels: Vec<usize>
+}
+
+impl Report {
+    fn new(input: &str) -> Self {
+        Self {
+            levels: input.split_whitespace()
+                .map(|level| level.parse().unwrap())
+                .collect()
+        }
+    }
+
+    fn get_safety_level(&self) -> SafetyLevel {
+        fn is_safe(levels: &[usize]) -> bool {
+            let diffs: Vec<(usize, Ordering)> = levels.windows(2)
+                .map(|w| (w[1].abs_diff(w[0]), w[1].cmp(&w[0])))
+                .collect();
+            diffs.iter().all(|&(v, o)|
+                (1..=3).contains(&v) && o == diffs.first().unwrap().1)
+        }
+        if is_safe(&self.levels) {
+            SafetyLevel::Safe
+        } else {
+            for n in 0..self.levels.len() {
+                let remove_one: Vec<usize> = self.levels.iter()
+                    .enumerate()
+                    .filter(|(i, _)| *i != n)
+                    .map(|(_, level)| *level)
+                    .collect();
+                if is_safe(&remove_one) {
+                    return SafetyLevel::DampenerSafe;
+                }
+            }
+            SafetyLevel::Unsafe
+        }
+    }
 }
 
 struct InputData {
-    reports: Vec<Vec<usize>>
+    reports: Vec<Report>
 }
 
 impl InputData {
     fn parse_input(input: &str) -> Self {
         Self {
             reports: input.lines()
-                .map(|line|
-                    line.split_whitespace()
-                    .map(|nbr| nbr.parse().unwrap())
-                    .collect())
+                .map(Report::new)
                 .collect()
         }
     }
 
-    fn solve_part1(&self) -> usize {
-        self.reports.iter()
-            .filter(|levels| is_safe(levels))
-            .count()
+    fn solve(&self) -> (usize, usize) {
+        let safety: Vec<SafetyLevel> = self.reports.iter()
+            .map(|r| r.get_safety_level())
+            .collect();
+        let safe: usize = safety.iter()
+            .filter(|s| **s == SafetyLevel::Safe)
+            .count();
+        let almost_safe: usize = safety.iter()
+            .filter(|s| **s == SafetyLevel::DampenerSafe)
+            .count();
+        (safe, safe + almost_safe)
     }
-
-    fn solve_part2(&self) -> usize {
-        self.reports.iter()
-            .filter(|levels|
-                if is_safe(levels) {
-                    true
-                } else {
-                    is_almost_safe(levels)
-                } )
-            .count()
-    }
-}
-
-fn is_safe(levels: &[usize]) -> bool {
-    let diffs: Vec<(usize, Ordering)> = levels.windows(2).map(|w| (w[1].abs_diff(w[0]), w[1].cmp(&w[0]))).collect();
-    diffs.iter().all(|&(v, o)| (1..=3).contains(&v) && o == diffs.first().unwrap().1)
-}
-
-fn is_almost_safe(levels: &[usize]) -> bool {
-    for n in 0..levels.len() {
-        let removed: Vec<usize> = levels.iter().enumerate().filter(|(i, _)| *i != n).map(|(_, level)| *level).collect();
-        if is_safe(&removed) {
-            return true;
-        }
-    }
-    false
 }
 
 
@@ -61,7 +86,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1_example_1() {
+    fn parts1_2_example_1() {
         let testdata = String::from(
 "7 6 4 2 1
 1 2 7 8 9
@@ -70,19 +95,8 @@ mod tests {
 8 6 4 4 1
 1 3 6 7 9");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part1(), 2);
-    }
-
-    #[test]
-    fn part2_example_1() {
-        let testdata = String::from(
-"7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9");
-        let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part2(), 4);
+        let (p1, p2) = solution_data.solve();
+        assert_eq!(p1, 2);
+        assert_eq!(p2, 4);
     }
 }
