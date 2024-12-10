2b93d2decc1c7e8814896d852656c1778692938b
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-12-10 18:25:34 +0100

    2024 day 10: huge time savings - no need to keep track of seen points in queue. Neighbors are all +1, so all paths explored are guaranteed to be unique.

--- src/year2024/day10.rs
+++ src/year2024/day10.rs
@@ -8,37 +8,53 @@ pub fn solve(input: &str) {
     println!("Part 2: {}", p2);
 }
 
-struct InputData {
-    map: Vec<Vec<usize>>,
+struct Grid {
     x_max: usize,
     y_max: usize,
+    elements: Vec<u8>
+}
+
+impl Grid {
+    #[inline]
+    fn parse(input: &str) -> Self {
+        let lines: Vec<_> = input.lines()
+            .map(|line| line.chars()
+                .map(|c| c.to_digit(10).unwrap() as u8)
+                .collect::<Vec<_>>())
+            .collect();
+        let x_max = lines[0].len();
+        let y_max = lines.len();
+        let mut elements = Vec::with_capacity(x_max * y_max);
+        lines.iter().for_each(|line| line.iter().for_each(|c| elements.push(*c)));
+        Self { x_max, y_max, elements }
+    }
+
+    #[inline]
+    fn get_element(&self, p: &Point) -> Option<u8> {
+        if (0..self.x_max).contains(&(p.x as usize)) && 
+                (0..self.y_max).contains(&(p.y as usize)) {
+            Some(self.elements[self.x_max * (p.y as usize) + (p.x as usize)])
+        } else {
+            None
+        }
+    }
+}
+
+struct InputData {
+    map: Grid,
     trailheads: Vec<Point>
 }
 
 impl InputData {
     fn parse_input(input: &str) -> Self {
-        let mut map = Vec::new();
-        let mut trailheads = Vec:: new();
-        let mut x_max = 0;
-        let mut y_max = 0;
-        for (y, line) in input.lines().enumerate() {
-            y_max += 1;
-            if y == 0 {
-                x_max = line.len();
-            }
-            map.push(Vec::with_capacity(line.len()));
-            for (x, c) in line.chars().enumerate() {
-                let val = c.to_digit(10).unwrap();
-                map[y].push(val as usize);
-                if val == 0 {
-                    trailheads.push(Point::new(x as i32, y as i32));
-                }
-            }
-        }
+        let map = Grid::parse(input);
+        let trailheads = map.elements.iter()
+            .enumerate()
+            .filter(|(_, v)| **v == 0)
+            .map(|(i, _)| Point::new((i % map.x_max) as i32, (i / map.y_max) as i32))
+            .collect();
         Self {
             map,
-            x_max,
-            y_max,
             trailheads
         }
     }
@@ -48,26 +64,18 @@ impl InputData {
         let mut totalrating = 0;
         for head in &self.trailheads {
             let mut peaks = HashSet::new();
-            let mut seen = HashSet::new();
-            let mut queue: VecDeque<(Point, Vec<Point>)> = VecDeque::new();
-            queue.push_back((*head, vec![*head]));
-            while let Some((current_point, current_seen)) = queue.pop_front() {
-                if seen.contains(&(current_point, current_seen.clone())) {
-                    continue;
-                }
-                seen.insert((current_point, current_seen.clone()));
-                if self.map[current_point.y as usize][current_point.x as usize] == 9 {
-                    peaks.insert(current_point);
+            let mut queue: VecDeque<Point> = VecDeque::new();
+            queue.push_back(*head);
+            while let Some(current) = queue.pop_front() {
+                if self.map.get_element(&current).unwrap() == 9 {
+                    peaks.insert(current);
                     totalrating += 1;
                 } else {
-                    for d in [LEFT, UP, RIGHT, DOWN] {
-                        let new_x = current_point.x + d.x;
-                        let new_y = current_point.y + d.y;
-                        if (0..self.x_max as i32).contains(&new_x) && (0..self.y_max as i32).contains(&new_y) &&
-                                self.map[new_y as usize][new_x as usize] == self.map[current_point.y as usize][current_point.x as usize] + 1 {
-                            let mut new_seen: Vec<Point> = current_seen.clone();
-                            new_seen.push(current_point);
-                            queue.push_back((Point::new(new_x, new_y), new_seen));
+                    for neighbor in NEIGHBORS_STRAIGHT.map(|dir| dir + current) {
+                        if let Some(neighbor_val) = self.map.get_element(&neighbor) {
+                            if neighbor_val == self.map.get_element(&current).unwrap() + 1 {
+                                queue.push_back(neighbor);
+                            }
                         }
                     }
                 }
