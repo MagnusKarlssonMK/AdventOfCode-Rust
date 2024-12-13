4ea02f250589e115fbb434abf929317662644c22
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2024-12-13 21:34:33 +0100

    2024 day 12 calculate both parts in the same loop. Simplify the way to count sides (corners) and do it while first visiting the node rather than a separate loop afterwards.

--- src/year2024/day12.rs
+++ src/year2024/day12.rs
@@ -1,12 +1,28 @@
+/*
+- Floodfill with BFS to find the groups.
+- For each point found during the search, add +1 to the area, and +1 to 
+  the perimeter for each orthogonal neighbor having a different value.
+
+For part 2, number of sides == number of corners.
+For each point found during the BFS, then for each corner:
+- If both orthogonal neighbors have the same value, then take a sneak peek on
+  the diagonal point; if it has a different value, add +1 to corners.
+- If both orthogonal neighbors have different value(s), add +1 to corners
+  (either the diagonal also has a different value -> it's a corner; or it's
+  the same value, which could mean either same or different group, and in
+  both cases it means we've found a corner.)
+- If only one of the orthogonal neighbors have the same value, we know
+  it's not a corner.
+*/
+
 use std::collections::{HashSet, VecDeque};
-
-
 use crate::aoc_util::point::*;
 
 pub fn solve(input: &str) {
     let solution_data = InputData::parse_input(input);
-    println!("Part 1: {}", solution_data.solve_part1());
-    println!("Part 2: {}", solution_data.solve_part2());
+    let (p1, p2) = solution_data.solve();
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
 }
 
 struct Grid {
@@ -51,50 +67,19 @@ impl InputData {
         }
     }
 
-    fn solve_part1(&self) -> usize {
-        let mut total = 0;
+    fn solve(&self) -> (usize, usize) {
+        let mut total_perimeter = 0;
+        let mut total_sides = 0;
         let mut counted = HashSet:: new();
         for y in 0..self.garden_map.y_max {
             for x in 0..self.garden_map.x_max {
                 if !counted.contains(&(x, y)) {
                     let mut area = 0;
                     let mut perimeter = 0;
-                    let mut queue = VecDeque::from([Point::new(x as i32, y as i32)]);
-                    while let Some(current) = queue.pop_front() {
-                        if counted.contains(&(current.x as usize, current.y as usize)) {
-                            continue;
-                        }
-                        counted.insert((current.x as usize, current.y as usize));
-                        area += 1;
-                        let element = self.garden_map.get_element(&current).unwrap();
-                        for neighbor in NEIGHBORS_STRAIGHT.map(|dir| dir + current) {
-                            if let Some(neighbor_val) = self.garden_map.get_element(&neighbor) {
-                                if neighbor_val == element {
-                                    queue.push_back(neighbor);
-                                } else {
-                                    perimeter += 1;
-                                }
-                            } else {
-                                perimeter += 1;
-                            }
-                        }
-                    }
-                    total += area * perimeter;
-                }
-            }
-        }
-        total
-    }
-
-    fn solve_part2(&self) -> usize {
-        let mut total = 0;
-        let mut counted = HashSet:: new();
-        for y in 0..self.garden_map.y_max {
-            for x in 0..self.garden_map.x_max {
-                if !counted.contains(&(x, y)) {
-                    let mut area = 0;
+                    let mut sides = 0;
                     let mut group = HashSet::new();
                     let mut queue = VecDeque::from([Point::new(x as i32, y as i32)]);
+                    let mut neighborstates = Vec::with_capacity(6);
                     while let Some(current) = queue.pop_front() {
                         if counted.contains(&(current.x as usize, current.y as usize)) {
                             continue;
@@ -103,39 +88,43 @@ impl InputData {
                         group.insert(current);
                         area += 1;
                         let element = self.garden_map.get_element(&current).unwrap();
-                        for neighbor in NEIGHBORS_STRAIGHT.map(|dir| dir + current) {
+                        for dir in NEIGHBORS_STRAIGHT.iter() {
+                            let neighbor = current + *dir;
                             if let Some(neighbor_val) = self.garden_map.get_element(&neighbor) {
                                 if neighbor_val == element {
                                     queue.push_back(neighbor);
+                                    neighborstates.push((*dir, 1));
                                 } else {
-                                    //perimeter += 1;
+                                    perimeter += 1;
+                                    neighborstates.push((*dir, 0));
                                 }
                             } else {
-                                //perimeter += 1;
+                                perimeter += 1;
+                                neighborstates.push((*dir, 0));
                             }
                         }
-                    }
-                    let mut sides_seen = HashSet::new();
-                    for current in group.iter() {
-                        for dir in &NEIGHBORS_STRAIGHT {
-                            let neighbor = *dir + *current;
-                            if group.contains(&neighbor) {
-                                continue;
-                            }
-                            let mut feeler = *current;
-                            while !group.contains(&(feeler + *dir)) && group.contains(&(feeler + Point::new(dir.y, dir.x))) {
-                                feeler = Point::new(feeler.x + dir.y, feeler.y + dir.x);
-                            }
-                            if !sides_seen.contains(&(feeler, *dir)) {
-                                sides_seen.insert((feeler, *dir));
+                        // Check the combinations of straight neighbors to evaluate corners
+                        for (i, (n1, v1)) in neighborstates.iter().enumerate() {
+                            for (n2, v2) in neighborstates.iter().skip(i) {
+                                if (n1.x == 0 && n2.x == 0) || (n1.y == 0 && n2.y == 0) {
+                                    continue;
+                                }
+                                if (*v1 == 0 && *v2 == 0) || (*v1 == 1 && *v2 == 1 && 
+                                        self.garden_map.get_element(
+                                            &Point::new(current.x + n1.x + n2.x,
+                                                        current.y + n1.y + n2.y)).unwrap() != element) {
+                                    sides += 1;
+                                }
                             }
                         }
+                        neighborstates.clear();
                     }
-                    total += area * sides_seen.len();
+                    total_perimeter += area * perimeter;
+                    total_sides += area * sides;
                 }
             }
         }
-        total
+        (total_perimeter, total_sides)
     }
 }
 
@@ -151,7 +140,8 @@ BBCD
 BBCC
 EEEC");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part1(), 140);
+        let (p1, _) = solution_data.solve();
+        assert_eq!(p1, 140);
     }
 
     #[test]
@@ -163,7 +153,8 @@ OOOOO
 OXOXO
 OOOOO");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part1(), 772);
+        let (p1, _) = solution_data.solve();
+        assert_eq!(p1, 772);
     }
 
     #[test]
@@ -180,7 +171,8 @@ MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part1(), 1930);
+        let (p1, _) = solution_data.solve();
+        assert_eq!(p1, 1930);
     }
 
     #[test]
@@ -191,7 +183,8 @@ BBCD
 BBCC
 EEEC");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part2(), 80);
+        let (_, p2) = solution_data.solve();
+        assert_eq!(p2, 80);
     }
 
     #[test]
@@ -203,7 +196,8 @@ EEEEE
 EXXXX
 EEEEE");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part2(), 236);
+        let (_, p2) = solution_data.solve();
+        assert_eq!(p2, 236);
     }
 
     #[test]
@@ -216,7 +210,8 @@ ABBAAA
 ABBAAA
 AAAAAA");
         let solution_data = InputData::parse_input(&testdata);
-        assert_eq!(solution_data.solve_part2(), 368);
+        let (_, p2) = solution_data.solve();
+        assert_eq!(p2, 368);
     }
 
 }
