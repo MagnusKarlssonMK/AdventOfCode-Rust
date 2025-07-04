7317c4309573131b83936cb2e6ffdb894f302035
MagnusKarlssonMK <155587399+MagnusKarlssonMK@users.noreply.github.com>
2025-07-04 09:21:45 +0200

    2023 day 1 - Fix for unlikely input lines containing no numbers or only one spelled out number. Some refactoring and added custom tests.

--- src/year2023/day01.rs
+++ src/year2023/day01.rs
@@ -17,6 +17,45 @@ struct InputData<'a> {
     data: Vec<&'a str>,
 }
 
+fn calculate_line(line: &str, spelled_out: bool) -> usize {
+    let mut chrline = line.as_bytes();
+    let mut first = None;
+    let mut last = None;
+
+    while first.is_none() && !chrline.is_empty() {
+        if chrline[0].is_ascii_digit() {
+            first = Some(chrline[0].wrapping_sub(b'0') as usize);
+        } else if spelled_out {
+            for (i, nbr) in SPELLED_OUT.iter().enumerate() {
+                if chrline.starts_with(nbr) {
+                    first = Some(i + 1);
+                    break;
+                }
+            }
+        }
+        if first.is_none() {
+            chrline = &chrline[1..];
+        }
+    }
+
+    while last.is_none() && !chrline.is_empty() {
+        if chrline[chrline.len() - 1].is_ascii_digit() {
+            last = Some(chrline[chrline.len() - 1].wrapping_sub(b'0') as usize);
+        } else if spelled_out {
+            for (i, nbr) in SPELLED_OUT.iter().enumerate() {
+                if chrline.ends_with(nbr) {
+                    last = Some(i + 1);
+                    break;
+                }
+            }
+        }
+        if last.is_none() {
+            chrline = &chrline[..chrline.len() - 1];
+        }
+    }
+    10 * first.unwrap_or(0) + last.unwrap_or(0)
+}
+
 impl<'a> InputData<'a> {
     fn parse_input(input: &'a str) -> Self {
         Self {
@@ -27,61 +66,14 @@ impl<'a> InputData<'a> {
     fn solve_part1(&self) -> usize {
         self.data
             .iter()
-            .map(|line| {
-                let first = line
-                    .chars()
-                    .find(char::is_ascii_digit)
-                    .unwrap()
-                    .to_digit(10)
-                    .unwrap();
-                let last = line
-                    .chars()
-                    .rfind(char::is_ascii_digit)
-                    .unwrap()
-                    .to_digit(10)
-                    .unwrap();
-                (10 * first + last) as usize
-            })
+            .map(|line| calculate_line(line, false))
             .sum()
     }
 
     fn solve_part2(&self) -> usize {
         self.data
             .iter()
-            .map(|line| {
-                let mut chrline = line.as_bytes();
-                let mut first: Option<usize> = None;
-                let mut last: Option<usize> = None;
-
-                while first.is_none() {
-                    if chrline[0].is_ascii_digit() {
-                        first = Some(chrline[0].wrapping_sub(b'0') as usize);
-                        break;
-                    }
-                    for (i, nbr) in SPELLED_OUT.iter().enumerate() {
-                        if chrline.starts_with(nbr) {
-                            first = Some(i + 1);
-                            break;
-                        }
-                    }
-                    chrline = &chrline[1..];
-                }
-
-                while last.is_none() {
-                    if chrline[chrline.len() - 1].is_ascii_digit() {
-                        last = Some(chrline[chrline.len() - 1].wrapping_sub(b'0') as usize);
-                        break;
-                    }
-                    for (i, nbr) in SPELLED_OUT.iter().enumerate() {
-                        if chrline.ends_with(nbr) {
-                            last = Some(i + 1);
-                            break;
-                        }
-                    }
-                    chrline = &chrline[..chrline.len() - 1];
-                }
-                10 * first.unwrap() + last.unwrap()
-            })
+            .map(|line| calculate_line(line, true))
             .sum()
     }
 }
@@ -92,15 +84,41 @@ mod tests {
 
     #[test]
     fn part1_example_1() {
-        let testdata = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
-        let solution_data = InputData::parse_input(testdata);
+        const TEST_DATA: &str = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        let solution_data = InputData::parse_input(TEST_DATA);
         assert_eq!(solution_data.solve_part1(), 142);
     }
 
     #[test]
+    fn part1_example_2() {
+        // Custom test to verify input with no numbers
+        const TEST_DATA: &str = "trebuchet";
+        let solution_data = InputData::parse_input(TEST_DATA);
+        assert_eq!(solution_data.solve_part1(), 0);
+    }
+
+    #[test]
     fn part2_example_1() {
-        let testdata = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen";
-        let solution_data = InputData::parse_input(testdata);
+        const TEST_DATA: &str = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        let solution_data = InputData::parse_input(TEST_DATA);
         assert_eq!(solution_data.solve_part2(), 281);
+    }
+
+    #[test]
+    fn part2_example_2() {
+        // Custom test to verify input with either no numbers, or only one spelled out number
+        const TEST_DATA: &str = "trebuchet
+abcdfiveefg";
+        let solution_data = InputData::parse_input(TEST_DATA);
+        assert_eq!(solution_data.solve_part2(), 55);
     }
 }
