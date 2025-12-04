64c7dd7fa13f79279f65b7b4f31c57da016b76f1
MagnusKarlssonMK <magnus.karlsson.mk@gmail.com>
2025-12-04 16:19:42 +0100

    2025 day 4 - improved faster solution.

--- src/year2025/day04.rs
+++ src/year2025/day04.rs
@@ -1,9 +1,23 @@
-//! # 2025 day 04 - Printing Department
+//! # 2025 day 4 - Printing Department
 //!
-//! Kind of brute-force for part 2, can probably be optimized by storing paper neighbors
-//! for every removed paper roll each round, and only check those points next iteration,
-//! instead of scanning the entire map every time.
-use std::{error::Error, str::FromStr};
+//! ## Part 1
+//! * Build a queue of the points containing paper ('@'), to minimize the number of points that
+//!   need to have their neighbors scanned.
+//! * Iterate over those points and check all 8 neighbors. If the number of neighbors containing
+//!   paper is less than 4, increment the total result.
+//!
+//! ## Part 2
+//! Despite initially looking like a game-of-life problem, it's actually not, since no nodes ever
+//! grow back to contain paper. So there is actually no need to iterate in rounds as in the
+//! example in the problem description. Instead, we can remove identified points immediately,
+//! and then add its neighbors back to the queue for re-evaluation. So,
+//!
+//! * Start similar to part 1, but also create a copy of the map to keep track of current state
+//!   as paper gets removed.
+//! * When a point is identified to be cleared, update that point in the map and push all its
+//!   neighbors containing paper to the back of the queue to be re-evaluated.
+//! * Once the queue is empty, count the number of nodes in the map that still contain paper.
+use std::{collections::VecDeque, error::Error, str::FromStr};
 
 use crate::aoc_util::{grid::Grid, point::*};
 
@@ -28,60 +42,69 @@ impl FromStr for InputData {
     }
 }
 
+const PAPER: char = '@';
+const NOT_PAPER: char = '.';
+
 impl InputData {
     fn solve_part1(&self) -> usize {
-        let mut count = 0;
-        for (i, c) in self.paper_map.elements.iter().enumerate() {
-            if *c == '@' {
-                let p = self.paper_map.get_point(i);
-                let mut neighbor_count = 0;
-                for neighbor in NEIGHBORS_ALL.iter().map(|n| *n + p) {
-                    if let Some(nc) = self.paper_map.get_element(&neighbor)
-                        && nc == '@'
-                    {
-                        neighbor_count += 1;
-                        if neighbor_count >= 4 {
-                            break;
-                        }
-                    }
-                }
-                if neighbor_count < 4 {
-                    count += 1;
+        let mut total = 0;
+        let paper: Vec<_> = self
+            .paper_map
+            .elements
+            .iter()
+            .enumerate()
+            .filter(|(_, c)| **c == PAPER)
+            .map(|(i, _)| self.paper_map.get_point(i))
+            .collect();
+        let mut queue = VecDeque::from(paper);
+        while let Some(p) = queue.pop_front() {
+            let mut paper_neighbors = 0;
+            for neighbor in NEIGHBORS_ALL.iter().map(|n| *n + p) {
+                if let Some(c) = self.paper_map.get_element(&neighbor)
+                    && c == PAPER
+                {
+                    paper_neighbors += 1;
                 }
             }
+            if paper_neighbors < 4 {
+                total += 1;
+            }
         }
-        count
+        total
     }
 
     fn solve_part2(&self) -> usize {
-        let mut count = self.paper_map.elements.len();
         let mut total = 0;
-        let mut current_map = self.paper_map.clone();
-        while count > 0 {
-            count = 0;
-            let mut next_map = current_map.clone();
-            for (i, c) in current_map.elements.iter().enumerate() {
-                if *c == '@' {
-                    let p = current_map.get_point(i);
-                    let mut neighbor_count = 0;
-                    for neighbor in NEIGHBORS_ALL.iter().map(|n| *n + p) {
-                        if let Some(nc) = current_map.get_element(&neighbor)
-                            && nc == '@'
-                        {
-                            neighbor_count += 1;
-                            if neighbor_count >= 4 {
-                                break;
-                            }
-                        }
-                    }
-                    if neighbor_count < 4 {
-                        count += 1;
-                        next_map.set_point(&p, '.');
-                    }
+        let mut paper_map = self.paper_map.clone();
+        let paper: Vec<_> = self
+            .paper_map
+            .elements
+            .iter()
+            .enumerate()
+            .filter(|(_, c)| **c == PAPER)
+            .map(|(i, _)| self.paper_map.get_point(i))
+            .collect();
+        let mut queue = VecDeque::from(paper);
+        while let Some(p) = queue.pop_front() {
+            if paper_map.get_element(&p).unwrap() != PAPER {
+                // The point has already been cleared after this entry got added to the queue
+                continue;
+            }
+            let mut paper_neighbors = Vec::with_capacity(8);
+            for neighbor in NEIGHBORS_ALL.iter().map(|n| *n + p) {
+                if let Some(c) = paper_map.get_element(&neighbor)
+                    && c == PAPER
+                {
+                    paper_neighbors.push(neighbor);
                 }
             }
-            total += count;
-            current_map = next_map;
+            if paper_neighbors.len() < 4 {
+                total += 1;
+                paper_map.set_point(&p, NOT_PAPER);
+                for np in &paper_neighbors {
+                    queue.push_back(*np);
+                }
+            }
         }
         total
     }
